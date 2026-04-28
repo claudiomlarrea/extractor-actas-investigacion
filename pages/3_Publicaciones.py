1a49ef234085d82b75165f477d7cd41501657d7d
Claudio Larrea <investigacion@uccuyo.edu.ar>
2026-04-28 08:55:07 -0300

    Update 3_Publicaciones.py

--- pages/3_Publicaciones.py
+++ pages/3_Publicaciones.py
@@ -3,7 +3,9 @@ import streamlit as st
 st.title("Carga de Publicaciones")
 
 tab1, tab2, tab3 = st.tabs([
-    "📰 Diarios",
     "📑 Revistas científicas",
     "📚 Libros / Capítulos"
+    "🗂️ Repositorios",
+    "🎓 Reuniones científicas",
+    "📰 Diarios",
 ])
