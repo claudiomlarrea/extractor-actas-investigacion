b25b54beab80d4fe23bd264b4b2ce937bd38083e
Claudio Larrea <investigacion@uccuyo.edu.ar>
2026-05-03 11:41:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ h1, h2, h3, p {
     padding: 8px 10px;
 }
 
-/* OPCIÓN ACTIVA */
+/* OPCIÓN ACTIVA (app) */
 [data-testid="stSidebarNav"] li[aria-current="page"] a {
     background-color: #064a3f !important;
     color: white !important;
